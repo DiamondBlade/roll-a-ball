d9c025b36a49af8757b19238b768a1d765283ece
xxdylanxxfire <43252057+xxdylanxxfire@users.noreply.github.com>
2018-09-23 18:23:11 -0400

    Add files via upload

---
+++ Assets/scripts/Player_control.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_control : MonoBehaviour{
+
+    public float speed;
+    public Text counttext;
+    public Text wintext;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setcounttext();
+        wintext.text = "";
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setcounttext();
+        }
+    }
+    void setcounttext ()
+    {
+        counttext.text = "Count: " + count.ToString();
+        if (count >= 11)
+        {
+            wintext.text = "You Win!";
+        }
+    }
+}
